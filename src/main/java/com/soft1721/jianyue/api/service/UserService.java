bf7c14ee6dcbace4e9a1ee0204d207eb37da98cf
1702343131 <473290621@qq.com>
2019-04-03 21:58:53 +0800

    jianyue-api

---
+++ src/main/java/com/soft1721/jianyue/api/service/UserService.java
@@ -0,0 +1,19 @@
+package com.soft1721.jianyue.api.service;
+
+import com.soft1721.jianyue.api.entity.User;
+import com.soft1721.jianyue.api.entity.dto.UserDTO;
+
+/**
+
+ * Created by 张文旭 on 2019/4/1.
+ */
+public interface UserService {
+    //根据手机号获取用户信息
+    User getUserByMobile(String mobile);
+    //登录方式 @param userDTO
+    int signIn(UserDTO userDTO);
+    //根据id获取用户信息
+    User getUserById(int id);
+    //修改用户
+    void updateUser(User user);
+}
---
+++ src/main/java/com/soft1721/jianyue/api/util/StatusConst.java
@@ -0,0 +1,12 @@
+package com.soft1721.jianyue.api.util;
+
+/**
+ * Created by 张文旭 on 2019/4/1.
+ */
+public class StatusConst {
+    public static final int SUCCESS = 0;
+    public static final int USER_ID_NOT_FOUND = 1;
+    public static final int USER_MOBILE_NOT_FOUND = 2;
+    public static final int PASSWORD_ERROR = 3;
+    public static final int USER_STATUS_ERROR = 4;
+}
